fadf52522c34ccecccd4f15929af41305ab841c5
soniSUHANI <soni03suhani@gmail.com>
2023-01-03 21:21:12 +0530

    practice

---
+++ if else qn.py
@@ -0,0 +1,13 @@
+age = int(input("enter your age: "))
+if age>18:
+    print("Yes")
+else:
+    print("No")
+#and = true if both operators are true else false
+# or = true if atleast any one operator is true else false
+# not = inverts true to false and false to true
+a = None
+if (a is None):
+    print("Yes")
+else:
+    print("No")
